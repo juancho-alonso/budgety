432a87f24cad1ac1a66376c339fa91cf501ce367
juancho-alonso <juan.agustin.alonso@gmail.com>
2020-08-17 15:40:32 +1000

    created Income and Expenses constructors

--- app.js
+++ app.js
@@ -2,6 +2,33 @@
 // BUDGET CONTROLLER
 var budgetController = (function(){
 
+    var Expense = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+
+    
+
+    var data = {
+        allItems:{
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    }
+
+
 })();
 
 // UI CONTROLLER
@@ -43,8 +70,6 @@ var controller = (function(budgetCtrl, UICtrl){
             }
         });
     }
-
-
 
     var ctrlAddItem = function(){
 
