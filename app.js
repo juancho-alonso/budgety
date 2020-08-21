d8919607fa25c9d80d523dfc93d36c005ea61ff3
juancho-alonso <juan.agustin.alonso@gmail.com>
2020-08-21 15:46:17 +1000

    Added current date display

--- app.js
+++ app.js
@@ -165,7 +165,8 @@ var UIController = (function () {
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container', 
-        expensesPercLabel: '.item__percentage'
+        expensesPercLabel: '.item__percentage',
+        dateLabel: '.budget__title--month'
     };
 
     var formatNumber = function(num, type){
@@ -279,6 +280,19 @@ var UIController = (function () {
             });
         },
 
+        displayMonth: function(){
+            var now, year, month, months;
+
+            now = new Date();
+
+            months = ['January', 'February', 'March', 'April', 'May', 'June',
+            'July', 'August', 'September', 'October', 'November', 'December'];
+            month = now.getMonth();
+
+            year = now.getFullYear();
+            document.querySelector(DOMStrings.dateLabel).textContent = months[month] + ' ' + year;
+
+        },
 
         getDOMStrings: function () {
             return DOMStrings;
@@ -383,6 +397,7 @@ var controller = (function (budgetCtrl, UICtrl) {
     return {
         init: function () {
             console.log('App started');
+            UICtrl.displayMonth();
             UICtrl.displayBudget(
                 {
                     budget: 0,
